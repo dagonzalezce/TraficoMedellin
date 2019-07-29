86693235843de88f13d3d8bcd290d2e75f2f4819
user.email <Juan@172.32.11.205>
2019-07-29 16:45:50 -0500

    Implementacion header interseccion

--- src/com/unalMed/TraficoMedellin/geometria/Interseccion.scala
+++
@@ -1,5 +0,0 @@
-package com.unalMed.TraficoMedellin.geometria
-
-class Interseccion( cx: Int , cy: Int, nombre : Option[String] = None ) extends Punto( cx, cy) {
-  
-}
--- src/com/unalMed/TraficoMedellin/vias/Interseccion.scala
+++ src/com/unalMed/TraficoMedellin/vias/Interseccion.scala
@@ -1,5 +1,6 @@
 package com.unalMed.TraficoMedellin.vias
+import com.unalMed.TraficoMedellin.geometria.Punto
 
-class Interseccion {
+class Interseccion( cx: Int , cy: Int, nombre : Option[String] = None ) extends Punto( cx, cy) {
   
 }
