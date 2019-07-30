cd757ef356e41ed2936473b87e3ceba14ed7d01c
sergioazg <53378284+sergioazg@users.noreply.github.com>
2019-07-30 13:03:11 -0500

    Update Movil.scala
    Modificación clase Movil

--- src/com/unalMed/TraficoMedellin/movil/Movil.scala
+++ src/com/unalMed/TraficoMedellin/movil/Movil.scala
@@ -1,5 +1,11 @@
 package com.unalMed.TraficoMedellin.movil
 
-class Movil {
+import com.unalMed.TraficoMedellin.geometria._
+
+abstract class Movil ( var posicion: Punto, var velocidad: Velocidad){
   
-}
+  def aumentaPosicion(p: Punto,veloc: Velocidad, dt: Int): Punto 
+  
+  def anguloDireccion = this.velocidad.angulo
+  
+} 
