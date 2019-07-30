b4259139b635441512200716947361b2009e9f65
juanardila <Juan@LAPTOP-HMBJC47P>
2019-07-30 14:30:43 -0500

    Encapsulacion de la clase Movil

--- src/com/unalMed/TraficoMedellin/movil/Movil.scala
+++ src/com/unalMed/TraficoMedellin/movil/Movil.scala
@@ -2,10 +2,21 @@ package com.unalMed.TraficoMedellin.movil
 
 import com.unalMed.TraficoMedellin.geometria._
 
-abstract class Movil ( var posicion: Punto, var velocidad: Velocidad){
+abstract class Movil ( private val _pos: Punto, private val _vel: Velocidad){
+  private var _posicion = new Punto( _pos.x, _pos.y )
+  private var _velocidad = new Velocidad( _vel.magnitud, new Angulo( _vel.angulo.angulo ) )
   
-  def aumentaPosicion(p: Punto,veloc: Velocidad, dt: Int): Punto 
+  def posicion = _posicion
+  def posicion_= (posicion: Punto): Unit = { _posicion.x = posicion.x
+                                             _posicion.y = posicion.y} 
   
-  def anguloDireccion = this.velocidad.angulo
+  def velocidad = _velocidad
+  
+  def velocidad_= (velocidad: Velocidad): Unit = { _velocidad.angulo = velocidad.angulo
+                                                   _velocidad.magnitud = velocidad.magnitud }
+  
+  def mover: Punto 
+  
+  def anguloDireccion : Angulo = _velocidad.angulo
   
 } 
