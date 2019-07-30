ba078d80d91c1e4c9e67f1cedf10223bc3e3c344
sergioazg <53378284+sergioazg@users.noreply.github.com>
2019-07-30 13:04:12 -0500

    Update MovimientoUniforme.scala
    Modificación Movimiento uniforme

--- src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
+++ src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
@@ -1,5 +1,19 @@
 package com.unalMed.TraficoMedellin.movil
 
+import com.unalMed.TraficoMedellin.geometria._
+import Math.cos
+import Math.sin
+
 trait MovimientoUniforme {
+  
+  
+  def aumentaPosicion(p: Punto,veloc: Velocidad, dt: Int): Punto ={
+    
+    val px = (p.x+(veloc.magnitud * Math.cos(veloc.angulo.angulo) * dt)).toInt
+    val py = (p.y+(veloc.magnitud * Math.sin(veloc.angulo.angulo) * dt)).toInt
+    val pn = new Punto(px,py)
+    pn
+    
+  }
   
 }
