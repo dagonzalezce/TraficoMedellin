e72327cb586b3e74c456a96010fd399b730ad4a4
Sergio <serandzg@hotmail.com>
2019-07-30 23:20:41 -0500

    Modificación movil y movimiento uniforme

--- src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
+++ src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
@@ -11,9 +11,9 @@ trait MovimientoUniforme {
   var _velocidad : Velocidad
   
   // https://es.wikipedia.org/wiki/Movimiento_rectil%C3%ADneo_uniforme
-  def mover : Unit = {
-    _posicion.x = ( _posicion.x.toDouble + _velocidad.magnitud * math.cos( _velocidad.angulo.angulo ) ).toInt
-    _posicion.y = ( _posicion.y.toDouble + _velocidad.magnitud + math.sin( _velocidad.angulo.angulo ) ).toInt
+  def mover (dt: Int): Unit = {
+    _posicion.x = ( _posicion.x.toDouble + _velocidad.magnitud * math.cos( _velocidad.angulo.angulo ) * dt ).toInt
+    _posicion.y = ( _posicion.y.toDouble + _velocidad.magnitud + math.sin( _velocidad.angulo.angulo ) * dt ).toInt
   }
     
   
