17667c38a5dae8719bc5572f8b8121ca708480ae
juanardila <Juan@172.32.35.202>
2019-07-31 08:54:04 -0500

    Corregido Movimiento uniforme y comentado el por que

--- src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
+++ src/com/unalMed/TraficoMedellin/movil/MovimientoUniforme.scala
@@ -11,9 +11,22 @@ trait MovimientoUniforme {
   var _velocidad : Velocidad
   
   // https://es.wikipedia.org/wiki/Movimiento_rectil%C3%ADneo_uniforme
-  def mover (dt: Int): Unit = {
-    _posicion.x = ( _posicion.x.toDouble + _velocidad.magnitud * math.cos( _velocidad.angulo.angulo ) * dt ).toInt
-    _posicion.y = ( _posicion.y.toDouble + _velocidad.magnitud + math.sin( _velocidad.angulo.angulo ) * dt ).toInt
+  /*
+   * dv/dt = 0
+   * Velocidad = constante
+   * V = vector velocidad ( constante )
+   * s0 = posicion inicial
+   * ds/dt = V => ds = Vdt => s = s0 - V*t
+   * descomponiendo en coordenadas
+   *  x = x0 - |V|*cos(V angulo en radianes)* t 
+   *  y = y0 - |V|*sin(V angulo en radianes)* t
+   *  t = 1 seg Reemplazando
+   *  x = x0 - |V|*cos(V angulo en radianes) 
+   *  y = y0 - |V|*sin(V angulo en radianes)
+   */
+  def mover : Unit = {
+    _posicion.x = ( _posicion.x.toDouble + _velocidad.magnitud * math.cos( _velocidad.angulo.angulo.toRadians ) ).toInt
+    _posicion.y = ( _posicion.y.toDouble + _velocidad.magnitud + math.sin( _velocidad.angulo.angulo.toRadians ) ).toInt
   }
     
   
