e3d60dd76d35e334c6766ed0a5e1294d92f25b5c
Mayra Tejada <mptejadao@unal.edu.co>
2019-08-01 18:14:55 -0500

    //Calculo de angulo / clase punto

--- src/com/unalMed/TraficoMedellin/geometria/Punto.scala
+++ src/com/unalMed/TraficoMedellin/geometria/Punto.scala
@@ -1,6 +1,6 @@
 package com.unalMed.TraficoMedellin.geometria
 
-class Punto ( private var _x: Int, private var _y: Int ) {
+case class Punto ( private var _x: Int, private var _y: Int ) {
   this.x_=( _x ) // Revisar los valores de x
   this.y_=( _y ) // Revisar los valores de y
   
@@ -9,5 +9,14 @@ class Punto ( private var _x: Int, private var _y: Int ) {
   
   def y = _y
   def y_= (y: Int): Unit = {if( y < 0 )_y = 0 else _y = y}
+  
+    def calcularAnguloA(punto: Punto): Double={
+    
+    var a=Math.atan2(punto.y-x, punto.x-x)*180.0/Math.PI
+    
+    if(a<0) a+=360
+    
+    a
+  }
   
 }
